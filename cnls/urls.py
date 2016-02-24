a809fef56855e0f6e4823171e58f8314f6f47ae7
Laure <laure.lmyv@gmail.com>
2016-02-24 21:30:47 +0000

    ajout robots.txt (disallow all)

--- cnls/urls.py
+++ cnls/urls.py
@@ -1,12 +1,11 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
-#    url(r'^actions/$', views.get_actions, name='actions'),
-#    url(r'^actions/title/(?P<title>[\w\ ]+)$', views.get_actions_by_title),
     url(r'^geoactions/$', views.get_geoactions, name='geoactions'),
-
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     #url(r'^faritra/$', views.get_faritra, name='faritra'),
 )
