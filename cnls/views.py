c44370c783e88293dd358255aa1ac799049387e0
Laure <laure.lmyv@gmail.com>
2016-03-11 04:50:07 +0000

    creee la structure url-views pour les pages de detail des actions

--- cnls/views.py
+++ cnls/views.py
@@ -1,9 +1,9 @@
 #-*- coding: utf-8 -*-
-
-#from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.core import serializers
+from django.shortcuts import get_object_or_404 #, render, redirect
+from django.db.models import get_model
 #from django.conf import settings as djangoSettings
 
 from .models import ActionNationale, ActionTananarive, ActionRegionale, ActionLocale, TypeIntervention, Cible
@@ -24,29 +24,18 @@ def home(request):
         'cibles' : Cible.objects.all(),        
         })))
 
-#def get_actions_by_title(request, title):
-#    actions = Action.objects.filter(titre=title)
-#    data = serializers.serialize('json', actions)
-#    return HttpResponse(data)    
-
+def detail(request, action_echelle, action_id):
+    template = loader.get_template('detail.html')
+    model = get_model('cnls', action_echelle)
+#    return HttpResponse("%s n° %s." % (action_echelle, action_id))
+    return HttpResponse(template.render(Context({'action' : get_object_or_404(model, pk=action_id)})))
+    
+# inusité
 def get_geoactions(request):
-#    actions = list(ActionRegionale.objects.all()) + list(ActionLocale.objects.all())
     actions = ActionLocale.objects.all()
     data = serializers.serialize('geojson', actions, srid='4326', use_natural_foreign_keys=True)
     return HttpResponse(data, content_type='application/json')
-"""
-def api_action(request, id):
-    if request.method == 'GET':
-        action = Action.objects.get(pk=id)
-        data = serializers.serialize('geojson', action)
-        return HttpResponse(data)
-    elif request.method == 'POST':
-        action = Action.objects.get(pk=id)
-        action.title = request.post.title
-        action.description = request.post.description
-        action.save()
-        return HttpResponse('OK')    
-"""
+
 #def get_faritra(request):
 #    faritra = open(djangoSettings.STATIC_ROOT + 'json/faritra.json', 'r')
 #    #faritra = serializers.serialize('json', data)
