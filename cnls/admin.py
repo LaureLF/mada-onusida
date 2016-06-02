f039ab03e85213479736080d7093c15e764f7a48
Laure <laure.lmyv@gmail.com>
2016-06-02 19:39:55 +0000

    classes 'a valider' seulement quand il y en a dans l'interface admin

--- cnls/admin.py
+++ cnls/admin.py
@@ -151,10 +151,14 @@ admin.site.register(ActionLocale,ActionLocaleAdmin)
 #admin.site.register(Faritra)
 #admin.site.register(Kaominina)
 #admin.site.register(Fokontany)
-admin.site.register(ActionNationaleAValider, ActionNationaleAValiderAdmin)
-admin.site.register(ActionTananariveAValider, ActionTananariveAValiderAdmin)
-admin.site.register(ActionRegionaleAValider, ActionRegionaleAValiderAdmin)
-admin.site.register(ActionLocaleAValider, ActionLocaleAValiderAdmin)
+if ActionNationale.objects.all().filter(validation='pasencore').exists():
+    admin.site.register(ActionNationaleAValider, ActionNationaleAValiderAdmin)
+if ActionTananarive.objects.all().filter(validation='pasencore').exists():
+    admin.site.register(ActionTananariveAValider, ActionTananariveAValiderAdmin)
+if ActionRegionale.objects.all().filter(validation='pasencore').exists():
+    admin.site.register(ActionRegionaleAValider, ActionRegionaleAValiderAdmin)
+if ActionLocale.objects.all().filter(validation='pasencore').exists():
+    admin.site.register(ActionLocaleAValider, ActionLocaleAValiderAdmin)
 
 
 ###########################
