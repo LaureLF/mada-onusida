65f088185e794f7f03f9b66c735893309b0eba21
LaureLF <LaureLF@users.noreply.github.com>
2016-09-12 15:14:50 -0400

    Ajoute des commentaires

--- cnls/geojson_serializer_with_id.py
+++ cnls/geojson_serializer_with_id.py
@@ -1,10 +1,14 @@
 from django.contrib.gis.serializers.geojson import Serializer as GeojsonSerializer
 
+# Personnalisation de la création des GeoJson
 class GeojsonWithIdSerializer(GeojsonSerializer):
         
     def get_dump_object(self, obj):
+        # Inclure le champ id (pas présent par défaut)
         self._current['id']=obj.pk
+        # Inclure le nom de la classe de l'objet
         self._current['classe']= obj.__class__.__name__
+        # Exclure un certain nombre de champs inutiles
         fields_to_remove = {'objectif', 'date_debut', 'date_fin', 'duree', 'createur', 'commentaire', 'montant_prevu', 'montant_disponible', 'devise', 'origine', 'contact', 'operateur', 'resultat_cf_annee_ant', 'priorite_psn', 'creation', 'maj', 'login_maj', 'validation'}
         for key in fields_to_remove:
             if key in self._current:
