b48ee3f8006c752cba0bf1e0bd9f99bbe9b2a9bb
LaureLF <LaureLF@users.noreply.github.com>
2016-09-12 21:02:49 -0400

    Corrige le bug sur le filtre par date

--- cnls/geojson_serializer_with_id.py
+++ cnls/geojson_serializer_with_id.py
@@ -9,7 +9,7 @@ class GeojsonWithIdSerializer(GeojsonSerializer):
         # Inclure le nom de la classe de l'objet
         self._current['classe']= obj.__class__.__name__
         # Exclure un certain nombre de champs inutiles
-        fields_to_remove = {'objectif', 'date_debut', 'date_fin', 'duree', 'createur', 'commentaire', 'montant_prevu', 'montant_disponible', 'devise', 'origine', 'contact', 'operateur', 'resultat_cf_annee_ant', 'priorite_psn', 'creation', 'maj', 'login_maj', 'validation'}
+        fields_to_remove = {'objectif', 'duree', 'createur', 'commentaire', 'montant_prevu', 'montant_disponible', 'devise', 'origine', 'contact', 'operateur', 'resultat_cf_annee_ant', 'priorite_psn', 'creation', 'maj', 'login_maj', 'validation'}
         for key in fields_to_remove:
             if key in self._current:
                 del self._current[key]
